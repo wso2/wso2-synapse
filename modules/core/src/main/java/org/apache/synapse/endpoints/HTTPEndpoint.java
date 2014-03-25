d60762559faa41dbae7ff895771b9cfbf0453db8
Vanjikumaran <vanjikumaran@gmail.com>
2014-03-25 13:55:51 +0530

    Removing the latest Change
    Latest Change has been reverted due to build break

--- modules/core/src/main/java/org/apache/synapse/endpoints/HTTPEndpoint.java
+++ modules/core/src/main/java/org/apache/synapse/endpoints/HTTPEndpoint.java
@@ -103,10 +103,7 @@ public class HTTPEndpoint extends AbstractEndpoint {
             } else if (httpMethod.equalsIgnoreCase(Constants.Configuration.HTTP_METHOD_HEAD)) {
                 synCtx.setProperty(Constants.Configuration.HTTP_METHOD,
                         Constants.Configuration.HTTP_METHOD_HEAD);
-            } else if (httpMethod.equalsIgnoreCase(Constants.Configuration.HTTP_METHOD_PATCH)) {
-                synCtx.setProperty(Constants.Configuration.HTTP_METHOD,
-                        Constants.Configuration.HTTP_METHOD_PATCH);                
-            }
+            } 
         }
     }
 
