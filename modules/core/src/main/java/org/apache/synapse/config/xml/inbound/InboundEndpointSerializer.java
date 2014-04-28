3d2af3ac49d9ddae1262f80bab2012d4caf15f5e
malakasilva <malaka.silva@gmail.com>
2014-04-28 13:57:01 +0530

    Build break fixed

--- modules/core/src/main/java/org/apache/synapse/config/xml/inbound/InboundEndpointSerializer.java
+++ modules/core/src/main/java/org/apache/synapse/config/xml/inbound/InboundEndpointSerializer.java
@@ -1,20 +1,20 @@
 /*
-*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * 
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 package org.apache.synapse.config.xml.inbound;
 
@@ -31,53 +31,51 @@ import java.util.Map;
 
 public class InboundEndpointSerializer {
 
-    private static final OMFactory fac = OMAbstractFactory.getOMFactory();
+	private static final OMFactory fac = OMAbstractFactory.getOMFactory();
 
-    protected static final OMNamespace nullNS
-            = fac.createOMNamespace(XMLConfigConstants.NULL_NAMESPACE, "");
+	protected static final OMNamespace nullNS = fac.createOMNamespace(XMLConfigConstants.NULL_NAMESPACE,
+	                                                                  "");
 
-    public static OMElement serializeInboundEndpoint(InboundEndpoint inboundEndpoint) {
+	public static OMElement serializeInboundEndpoint(InboundEndpoint inboundEndpoint) {
 
-        OMElement inboundEndpointElt = fac.createOMElement(InboundEndpointConstants.INBOUND_ENDPOINT,
-                SynapseConstants.SYNAPSE_OMNAMESPACE);
-        inboundEndpointElt.addAttribute(InboundEndpointConstants.INBOUND_ENDPOINT_NAME, inboundEndpoint.getName(), null);
-        inboundEndpointElt.addAttribute(InboundEndpointConstants.INBOUND_ENDPOINT_INTERVAL,
-                Long.toString(inboundEndpoint.getInterval()), null);
+		OMElement inboundEndpointElt = fac.createOMElement(InboundEndpointConstants.INBOUND_ENDPOINT,
+		                                                   SynapseConstants.SYNAPSE_OMNAMESPACE);
+		inboundEndpointElt.addAttribute(InboundEndpointConstants.INBOUND_ENDPOINT_NAME,
+		                                inboundEndpoint.getName(), null);
+		inboundEndpointElt.addAttribute(InboundEndpointConstants.INBOUND_ENDPOINT_INTERVAL,
+		                                Long.toString(inboundEndpoint.getInterval()), null);
+		inboundEndpointElt.addAttribute(InboundEndpointConstants.INBOUND_ENDPOINT_SEQUENCE,
+		                                inboundEndpoint.getInjectingSeq(), null);
+		inboundEndpointElt.addAttribute(InboundEndpointConstants.INBOUND_ENDPOINT_ERROR_SEQUENCE,
+		                                inboundEndpoint.getOnErrorSeq(), null);
+		inboundEndpointElt.addAttribute(InboundEndpointConstants.INBOUND_ENDPOINT_PROTOCOL,
+		                                inboundEndpoint.getProtocol(), null);
+		inboundEndpointElt.addAttribute(InboundEndpointConstants.INBOUND_ENDPOINT_SUSPEND,
+		                                Boolean.toString(inboundEndpoint.isSuspend()), null);
 
-        inboundEndpointElt.addAttribute(InboundEndpointConstants.INBOUND_ENDPOINT_PROTOCOL,
-                inboundEndpoint.getProtocol(), null);
-        inboundEndpointElt.addAttribute(InboundEndpointConstants.INBOUND_ENDPOINT_SUSPEND,
-                Boolean.toString(inboundEndpoint.isSuspend()), null);
+		OMElement parametersElt = fac.createOMElement(InboundEndpointConstants.INBOUND_ENDPOINT_PARAMETERS,
+		                                              SynapseConstants.SYNAPSE_OMNAMESPACE);
 
-        OMElement parametersElt = fac.createOMElement(InboundEndpointConstants.INBOUND_ENDPOINT_PARAMETERS,
-                SynapseConstants.SYNAPSE_OMNAMESPACE);
+		for (Map.Entry<String, String> paramEntry : inboundEndpoint.getParametersMap().entrySet()) {
+			OMElement parameter = fac.createOMElement(InboundEndpointConstants.INBOUND_ENDPOINT_PARAMETER,
+			                                          SynapseConstants.SYNAPSE_OMNAMESPACE);
+			parameter.addAttribute(InboundEndpointConstants.INBOUND_ENDPOINT_PARAMETER_NAME,
+			                       paramEntry.getKey(), null);
+			parameter.setText(paramEntry.getValue());
+			parametersElt.addChild(parameter);
+		}
 
-        for (Map.Entry<String, String> paramEntry : inboundEndpoint.getParametersMap().entrySet()) {
-            OMElement parameter = fac.createOMElement(InboundEndpointConstants.INBOUND_ENDPOINT_PARAMETER,
-                    SynapseConstants.SYNAPSE_OMNAMESPACE);
-            parameter.addAttribute(InboundEndpointConstants.INBOUND_ENDPOINT_PARAMETER_NAME, paramEntry.getKey(), null);
-            parameter.setText(paramEntry.getValue());
-            parametersElt.addChild(parameter);
-        }
+		inboundEndpointElt.addChild(parametersElt);
 
-        inboundEndpointElt.addChild(parametersElt);
+		return inboundEndpointElt;
+	}
 
-        OMElement sequenceElt = fac.createOMElement(InboundEndpointConstants.INBOUND_ENDPOINT_SEQ,
-                SynapseConstants.SYNAPSE_OMNAMESPACE);
-        sequenceElt.addAttribute(InboundEndpointConstants.INBOUND_ENDPOINT_SEQ_KEY, inboundEndpoint.getInjectingSeq(), null);
-        sequenceElt.addAttribute(InboundEndpointConstants.INBOUND_ENDPOINT_ON_ERROR_SEQ, inboundEndpoint.getOnErrorSeq(), null);
-
-        inboundEndpointElt.addChild(sequenceElt);
-
-        return inboundEndpointElt;
-    }
-
-
-    public static OMElement serializeInboundEndpoint(OMElement parent, InboundEndpoint inboundEndpoint) {
-        OMElement inboundEndpointElt = serializeInboundEndpoint(inboundEndpoint);
-        if (parent != null) {
-            parent.addChild(inboundEndpointElt);
-        }
-        return inboundEndpointElt;
-    }
+	public static OMElement serializeInboundEndpoint(OMElement parent,
+	                                                 InboundEndpoint inboundEndpoint) {
+		OMElement inboundEndpointElt = serializeInboundEndpoint(inboundEndpoint);
+		if (parent != null) {
+			parent.addChild(inboundEndpointElt);
+		}
+		return inboundEndpointElt;
+	}
 }
