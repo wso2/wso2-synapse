abd24ad1805558bd8407c3b15c756c9368ce8e04
Malaka <malaka.silva@gmail.com>
2014-09-04 10:07:20 +0530

    Revert "Revert "Fix ESBJAVA-3211""

--- modules/core/src/main/java/org/apache/synapse/message/senders/blocking/BlockingMsgSender.java
+++ modules/core/src/main/java/org/apache/synapse/message/senders/blocking/BlockingMsgSender.java
@@ -30,6 +30,7 @@ import org.apache.axis2.description.AxisOperation;
 import org.apache.axis2.description.AxisService;
 import org.apache.axis2.description.AxisServiceGroup;
 import org.apache.axis2.description.WSDL2Constants;
+import org.apache.axis2.transport.http.HTTPConstants;
 import org.apache.axis2.wsdl.WSDLConstants;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -121,8 +122,11 @@ public class BlockingMsgSender {
 
         axisOutMsgCtx.setConfigurationContext(configurationContext);
         axisOutMsgCtx.setEnvelope(axisInMsgCtx.getEnvelope());
-
-        // Fill MessageContext
+        axisOutMsgCtx.setProperty(HTTPConstants.NON_ERROR_HTTP_STATUS_CODES,
+                axisInMsgCtx.getProperty(HTTPConstants.NON_ERROR_HTTP_STATUS_CODES));
+        axisOutMsgCtx.setProperty(HTTPConstants.ERROR_HTTP_STATUS_CODES,
+                axisInMsgCtx.getProperty(HTTPConstants.ERROR_HTTP_STATUS_CODES));       
+		// Fill MessageContext
         BlockingMsgSenderUtils.fillMessageContext(endpointDefinition, axisOutMsgCtx, synapseInMsgCtx);
         if (JsonUtil.hasAJsonPayload(axisInMsgCtx)) {
             JsonUtil.cloneJsonPayload(axisInMsgCtx, axisOutMsgCtx);
@@ -151,7 +155,7 @@ public class BlockingMsgSender {
                 sendRobust(axisOutMsgCtx, clientOptions, anonymousService, serviceCtx);
             } else {
                 org.apache.axis2.context.MessageContext result =
-                        sendReceive(axisOutMsgCtx, clientOptions, anonymousService, serviceCtx);
+                sendReceive(axisOutMsgCtx, clientOptions, anonymousService, serviceCtx);               
                 synapseInMsgCtx.setEnvelope(result.getEnvelope());
                 if (JsonUtil.hasAJsonPayload(result)) {
                 	JsonUtil.cloneJsonPayload(result, ((Axis2MessageContext) synapseInMsgCtx).getAxis2MessageContext());
