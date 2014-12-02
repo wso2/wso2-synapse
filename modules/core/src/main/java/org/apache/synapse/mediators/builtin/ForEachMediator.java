acab0d1fe740a3a763e03c9197b7a07aaf160fcc
Maheeka Jayasuriya <maheeka@wso2.com>
2014-12-02 11:46:05 +0530

    splitting by xpath with namespaces

--- modules/core/src/main/java/org/apache/synapse/mediators/builtin/ForEachMediator.java
+++ modules/core/src/main/java/org/apache/synapse/mediators/builtin/ForEachMediator.java
@@ -1,26 +1,57 @@
 package org.apache.synapse.mediators.builtin;
 
+import java.util.List;
+
+import org.apache.axiom.soap.SOAPEnvelope;
 import org.apache.synapse.MessageContext;
 import org.apache.synapse.SynapseLog;
 import org.apache.synapse.mediators.AbstractMediator;
+import org.apache.synapse.mediators.eip.EIPUtils;
+import org.apache.synapse.util.MessageHelper;
 import org.apache.synapse.util.xpath.SynapseXPath;
-
+import org.jaxen.JaxenException;
 
 public class ForEachMediator extends AbstractMediator {
 
-	 /** The XPath that will list the elements to be splitted */
-    private SynapseXPath expression = null;
-    
+	/** The XPath that will list the elements to be splitted */
+	private SynapseXPath expression = null;
+
 	public boolean mediate(MessageContext synCtx) {
 		SynapseLog synLog = getLog(synCtx);
-		synLog.traceOrDebug("Start : For Each mediator");
-		if(expression!=null){
-		synLog.traceOrDebug("ForEach: expression = " + expression.toString());
+		
+		if (synLog.isTraceOrDebugEnabled()) {
+			synLog.traceOrDebug("Start : Foreach mediator");
+			synLog.traceTrace("Message : " + synCtx.getEnvelope());
+
+			if (synLog.isTraceTraceEnabled()) {
+				synLog.traceTrace("Message : " + synCtx.getEnvelope());
+			}
 		}
-		else{
+		
+		if (expression != null) {
+			synLog.traceOrDebug("ForEach: expression = " + expression.toString());
+		} else {
 			synLog.traceOrDebug("ForEach: expression is null");
 		}
 		
+		
+            try {
+	            // get a copy of the message for the processing, if the continueParent is set to true
+	            // this original message can go in further mediations and hence we should not change
+	            // the original message context
+	            SOAPEnvelope envelope = MessageHelper.cloneSOAPEnvelope(synCtx.getEnvelope());
+	            // get the iteration elements and iterate through the list,
+	            // this call will also detach all the iteration elements 
+	            List splitElements =
+	                                 EIPUtils.getDetachedMatchingElements(envelope, synCtx,
+	                                                                      expression);
+	            if (synLog.isTraceOrDebugEnabled()) {
+		            synLog.traceOrDebug("Splitting with XPath : " + expression + " resulted in " +
+		                                splitElements.size() + " elements");
+	            }
+            } catch  (JaxenException e) {
+                handleException("Error evaluating split XPath expression : " + expression, e, synCtx);
+            }
 		synLog.traceOrDebug("End : For Each mediator");
 		return true;
 	}
