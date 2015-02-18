534fd6f9cf70cdb528a4e914c9101e027a414f16
Pasadi <pasadim@wso2.com>
2015-02-18 19:39:17 +0530

    Alignment Change

--- modules/core/src/main/java/org/apache/synapse/mediators/AbstractListMediator.java
+++ modules/core/src/main/java/org/apache/synapse/mediators/AbstractListMediator.java
@@ -24,12 +24,8 @@ import org.apache.synapse.Mediator;
 import org.apache.synapse.MessageContext;
 import org.apache.synapse.SequenceType;
 import org.apache.synapse.SynapseLog;
-import org.apache.synapse.config.xml.MediatorSerializer;
 import org.apache.synapse.core.SynapseEnvironment;
 import org.apache.synapse.core.axis2.Axis2MessageContext;
-import org.apache.synapse.core.axis2.ProxyService;
-import org.apache.synapse.core.messagestore.Storage;
-import org.apache.synapse.mediators.base.SequenceMediator;
 import org.apache.synapse.mediators.collector.*;
 import org.apache.synapse.transport.passthru.util.RelayUtils;
 
@@ -42,181 +38,177 @@ import java.util.List;
  * @see ListMediator
  */
 public abstract class AbstractListMediator extends AbstractMediator
-        implements ListMediator {
+		implements ListMediator {
 
 	private TreeNode current;
-    /** the list of child mediators held. These are executed sequentially */
-    protected final List<Mediator> mediators = new ArrayList<Mediator>();
+	/**
+	 * the list of child mediators held. These are executed sequentially
+	 */
+	protected final List<Mediator> mediators = new ArrayList<Mediator>();
 
-    private boolean contentAware = false;
+	private boolean contentAware = false;
 	private SequenceType sequenceType = SequenceType.NAMED;
 
-    public boolean mediate(MessageContext synCtx) {
-        return  mediate(synCtx,0);
-    }
+	public boolean mediate(MessageContext synCtx) {
+		return mediate(synCtx, 0);
+	}
 
-    public boolean mediate(MessageContext synCtx, int mediatorPosition) {
-
-		if(CollectorEnabler.checkCollectorRequired()){
+	public boolean mediate(MessageContext synCtx, int mediatorPosition) {
+		if (CollectorEnabler.checkCollectorRequired()) {
 			// Set the parent mediator that is subjected to execute its set of children mediators as the current node of this instance
 			current = synCtx.getCurrent();
 		}
 
-        boolean returnVal = true;
-        int parentsEffectiveTraceState = synCtx.getTracingState();
-        // if I have been explicitly asked to enable or disable tracing, set it to the message
-        // to pass it on; else, do nothing -> i.e. let the parents state flow
-        setEffectiveTraceState(synCtx);
-        int myEffectiveTraceState = synCtx.getTracingState();
+		boolean returnVal = true;
+		int parentsEffectiveTraceState = synCtx.getTracingState();
+		// if I have been explicitly asked to enable or disable tracing, set it to the message
+		// to pass it on; else, do nothing -> i.e. let the parents state flow
+		setEffectiveTraceState(synCtx);
+		int myEffectiveTraceState = synCtx.getTracingState();
 
-        try {
-            SynapseLog synLog = getLog(synCtx);
-            if (synLog.isTraceOrDebugEnabled()) {
-                synLog.traceOrDebug("Sequence <" + getType() + "> :: mediate()");
-                synLog.traceOrDebug("Mediation started from mediator position : " + mediatorPosition);
-            }
+		try {
+			SynapseLog synLog = getLog(synCtx);
+			if (synLog.isTraceOrDebugEnabled()) {
+				synLog.traceOrDebug("Sequence <" + getType() + "> :: mediate()");
+				synLog.traceOrDebug("Mediation started from mediator position : " + mediatorPosition);
+			}
+			if (contentAware) {
+				try {
+					if (synLog.isTraceOrDebugEnabled()) {
+						synLog.traceOrDebug("Building message. Sequence <" + getType() + "> is content aware");
+					}
+					RelayUtils.buildMessage(((Axis2MessageContext) synCtx).getAxis2MessageContext(), false);
+				} catch (Exception e) {
+					handleException("Error while building message", e, synCtx);
+				}
+			}
 
-            if (contentAware) {
-                try {
-                    if (synLog.isTraceOrDebugEnabled()) {
-                        synLog.traceOrDebug("Building message. Sequence <" + getType() + "> is content aware");
-                    }
-                    RelayUtils.buildMessage(((Axis2MessageContext) synCtx).getAxis2MessageContext(),false);
-                } catch (Exception e) {
-                    handleException("Error while building message", e, synCtx);
-                }
-            }
-
-            for (int i = mediatorPosition; i < mediators.size(); i++) {
-                // ensure correct trace state after each invocation of a mediator
-                synCtx.setTracingState(myEffectiveTraceState);
-
+			for (int i = mediatorPosition; i < mediators.size(); i++) {
+				// ensure correct trace state after each invocation of a mediator
+				synCtx.setTracingState(myEffectiveTraceState);
 				// If the current mediator is incapable of containing child mediators then it is added as an leaf-node
-				if(CollectorEnabler.checkCollectorRequired()){
+				if (CollectorEnabler.checkCollectorRequired()) {
 					if (!MediatorData.hasList(mediators.get(i))) {
 						MediatorData.createNewSingleMediator(synCtx,
-								mediators.get(i));
+						                                     mediators.get(i));
 					}
 				}
-
-                if (!mediators.get(i).mediate(synCtx)) {
-                    returnVal = false;
-                    break;
-                }else{
-
+				if (!mediators.get(i).mediate(synCtx)) {
+					returnVal = false;
+					break;
+				} else {
 					if (CollectorEnabler.checkCollectorRequired()) {
 						// To remove in/out mediators in a scenario where 'in mediator' occur in a response and 'out mediator' occur in a request
 						if (current.getChildren() != null
-								&& !"".equals(current.getChildren())) {
-                            if(current.getLastChild()!=null) {
-
-                                if (!"".equals(current.getLastChild().getContents() // The name of the mediator is collected by getType() method. Since property mediator etc. override this method, the name is set as null
-                                        .getMediatorName())
-                                        && current.getLastChild().getContents()
-                                        .getMediatorName() != null
-                                        && current.getLastChild().getContents()
-                                        .getMediatorName()
-                                        .equals("Skipped")) {
-
-                                    synCtx.getCurrent()
-                                            .getChildren()
-                                            .remove(synCtx.getCurrent()
-                                                    .getLastChild());
-                }
-                                // if the executed child mediator is not removed then set  its ending time
-                                else {
-                                    MediatorData.setEndingTime(current
-                                            .getLastChild());
-
-            }
-                            }
+						    && !"".equals(current.getChildren())) {
+							if (current.getLastChild() != null) {
+								if (!"".equals(
+										current.getLastChild().getContents() // The name of the mediator is collected by getType() method. Since property mediator etc. override this method, the name is set as null
+												.getMediatorName())
+								    && current.getLastChild().getContents()
+								              .getMediatorName() != null
+								    && current.getLastChild().getContents()
+								              .getMediatorName()
+								              .equals("Skipped")) {
+									synCtx.getCurrent()
+									      .getChildren()
+									      .remove(synCtx.getCurrent()
+									                    .getLastChild());
+								}
+								// if the executed child mediator is not removed then set  its ending time
+								else {
+									MediatorData.setEndingTime(current
+											                           .getLastChild());
+								}
+							}
 						}
 						current = synCtx.getCurrent();
-						synLog.traceOrDebug("********** Printing the current node after execution of the mediator from AbstractListMediator : "
+						synLog.traceOrDebug(
+								"********** Printing the current node after execution of the mediator from AbstractListMediator : "
 								+ current.getContents().getMediatorName());
 					}
-
 				}
-            }
-        } finally {
-            synCtx.setTracingState(parentsEffectiveTraceState);
-        }
+			}
+		} finally {
+			synCtx.setTracingState(parentsEffectiveTraceState);
+		}
+		return returnVal;
+	}
 
-        return returnVal;
-    }
+	public List<Mediator> getList() {
+		return mediators;
+	}
 
-    public List<Mediator> getList() {
-        return mediators;
-    }
+	public boolean addChild(Mediator m) {
+		return mediators.add(m);
+	}
 
-    public boolean addChild(Mediator m) {
-        return mediators.add(m);
-    }
+	public boolean addAll(List<Mediator> c) {
+		return mediators.addAll(c);
+	}
 
-    public boolean addAll(List<Mediator> c) {
-        return mediators.addAll(c);
-    }
+	public Mediator getChild(int pos) {
+		return mediators.get(pos);
+	}
 
-    public Mediator getChild(int pos) {
-        return mediators.get(pos);
-    }
+	public boolean removeChild(Mediator m) {
+		return mediators.remove(m);
+	}
 
-    public boolean removeChild(Mediator m) {
-        return mediators.remove(m);
-    }
-
-    public Mediator removeChild(int pos) {
-        return mediators.remove(pos);
-    }
+	public Mediator removeChild(int pos) {
+		return mediators.remove(pos);
+	}
 
 	public void setSequenceType(SequenceType sequenceType) {
 		this.sequenceType = sequenceType;
 	}
 
-    /**
-     * Initialize child mediators recursively
-     * @param se synapse environment
-     */
-    public void init(SynapseEnvironment se) {
-        if (log.isDebugEnabled()) {
-            log.debug("Initializing child mediators of mediator : " + getType());
-        }
+	/**
+	 * Initialize child mediators recursively
+	 *
+	 * @param se synapse environment
+	 */
+	public void init(SynapseEnvironment se) {
+		if (log.isDebugEnabled()) {
+			log.debug("Initializing child mediators of mediator : " + getType());
+		}
 
-        for (int i = 0; i < mediators.size(); i++) {
-            Mediator mediator = mediators.get(i);
-            mediator.setMediatorPosition(i);
+		for (int i = 0; i < mediators.size(); i++) {
+			Mediator mediator = mediators.get(i);
+			mediator.setMediatorPosition(i);
 
-            if (mediator instanceof ManagedLifecycle) {
-                ((ManagedLifecycle) mediator).init(se);
-            }
+			if (mediator instanceof ManagedLifecycle) {
+				((ManagedLifecycle) mediator).init(se);
+			}
 
-            if (mediator.isContentAware()) {
-                if (log.isDebugEnabled()) {
-                    log.debug(mediator.getType() + " is content aware, setting sequence <" + getType() + "> as content aware");
-                }
-                contentAware = true;
-            }
-        }
-    }
+			if (mediator.isContentAware()) {
+				if (log.isDebugEnabled()) {
+					log.debug(mediator.getType() + " is content aware, setting sequence <" + getType() +
+					          "> as content aware");
+				}
+				contentAware = true;
+			}
+		}
+	}
 
-    /**
-     * Destroy child mediators recursively
-     */
-    public void destroy() {
-        if (log.isDebugEnabled()) {
-            log.debug("Destroying child mediators of mediator : " + getType());
-        }
+	/**
+	 * Destroy child mediators recursively
+	 */
+	public void destroy() {
+		if (log.isDebugEnabled()) {
+			log.debug("Destroying child mediators of mediator : " + getType());
+		}
 
-        for (Mediator mediator : mediators) {
-            if (mediator instanceof ManagedLifecycle) {
-                ((ManagedLifecycle) mediator).destroy();
-            }
-        }
-    }
+		for (Mediator mediator : mediators) {
+			if (mediator instanceof ManagedLifecycle) {
+				((ManagedLifecycle) mediator).destroy();
+			}
+		}
+	}
 
-    @Override
-    public boolean isContentAware() {
-        return contentAware;
-    }
+	@Override
+	public boolean isContentAware() {
+		return contentAware;
+	}
 
 }
