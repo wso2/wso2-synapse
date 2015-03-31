8daf24e21f9a32994fbf4525475897fdec0d5e42
ishan <ishan@wso2.com>
2015-03-31 12:23:15 +0530

    Adding several parameters to message store producers.

--- modules/core/src/main/java/org/apache/synapse/message/store/impl/commons/StorableMessage.java
+++ modules/core/src/main/java/org/apache/synapse/message/store/impl/commons/StorableMessage.java
@@ -24,11 +24,13 @@ import java.io.Serializable;
  * This represents the final message that will be saved in the storage queue.
  */
 public class StorableMessage implements Serializable {
+    private static final int PRIORITY_UNSET = -999;
+
     private Axis2Message axis2message;
 
     private SynapseMessage synapseMessage;
 
-    private int priority = 4;
+    private int priority = PRIORITY_UNSET;
 
     public Axis2Message getAxis2message() {
         return axis2message;
@@ -46,6 +48,17 @@ public class StorableMessage implements Serializable {
         this.synapseMessage = synapseMessage;
     }
 
+    public int getPriority(int defaultValue) {
+        if (priority == PRIORITY_UNSET) {
+            return defaultValue;
+        }
+        return priority;
+    }
+
+    /**
+     * @Depricated
+     * @return
+     */
     public int getPriority() {
         return priority;
     }
