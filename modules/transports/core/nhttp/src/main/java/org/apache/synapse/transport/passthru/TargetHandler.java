0f8795ba86a02b2f105f7ed5819fe72486f9f2b4
Senduran <bsenduran@gmail.com>
2016-02-02 15:09:25 +0530

    fixing esbjava4394

--- modules/transports/core/nhttp/src/main/java/org/apache/synapse/transport/passthru/TargetHandler.java
+++ modules/transports/core/nhttp/src/main/java/org/apache/synapse/transport/passthru/TargetHandler.java
@@ -485,7 +485,6 @@ public class TargetHandler implements NHttpClientEventHandler {
             log.warn("Connection closed by target host while receiving the response");
             isFault = true;
         } else if (state == ProtocolState.REQUEST_DONE) {
-            informWriterError(conn);
             log.warn("Connection closed by target host before receiving the response");
             isFault = true;
         }
@@ -660,7 +659,6 @@ public class TargetHandler implements NHttpClientEventHandler {
             informReaderError(conn);
             log.warn("Exception occured while reading the response");
         } else if (state == ProtocolState.REQUEST_DONE) {
-            informWriterError(conn);
             log.warn("Exception occured while before reading the response");
         }
         
