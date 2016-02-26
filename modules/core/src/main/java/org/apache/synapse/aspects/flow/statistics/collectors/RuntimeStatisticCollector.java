25d6b160794e01fbf9d8480d55dd512c8694429d
virajsenevirathne <viraj.uom@gmail.com>
2016-02-26 21:54:41 +0530

    Revert "Removing Testing code"
    This reverts commit 46952e5188c5af82f8a45abdc796a13596cb4813.

--- modules/core/src/main/java/org/apache/synapse/aspects/flow/statistics/collectors/RuntimeStatisticCollector.java
+++ modules/core/src/main/java/org/apache/synapse/aspects/flow/statistics/collectors/RuntimeStatisticCollector.java
@@ -328,6 +328,8 @@ public abstract class RuntimeStatisticCollector {
 			}
 			statisticDataUnit.getSynapseEnvironment().getCompletedStatisticStore()
 			                 .putCompletedStatisticEntry(statisticsEntry.getMessageFlowLogs());
+			statisticDataUnit.getSynapseEnvironment().getCompletedStatisticStore().putCompletedStatisticEntryForTesting(
+					statisticsEntry.getMessageFlowLogsForStatisticTesting());
 			runtimeStatistics.remove(statisticDataUnit.getStatisticId());
 		}
 	}
