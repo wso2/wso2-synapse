a47c5a8957f6288029638bbab7ec32b353c5c46c
virajsenevirathne <viraj.uom@gmail.com>
2016-05-03 11:38:24 +0530

    Changing completed store

--- modules/core/src/main/java/org/apache/synapse/aspects/flow/statistics/store/CompletedStatisticStore.java
+++ modules/core/src/main/java/org/apache/synapse/aspects/flow/statistics/store/CompletedStatisticStore.java
@@ -18,42 +18,50 @@
 
 package org.apache.synapse.aspects.flow.statistics.store;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.synapse.aspects.flow.statistics.publishing.PublishingFlow;
+import org.apache.synapse.aspects.flow.statistics.util.StatisticsConstants;
+import org.apache.synapse.config.SynapsePropertiesLoader;
 
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Queue;
 
 /**
  * This class will hold completed statistic entries till they are collected for by carbon mediation.
  */
 public class CompletedStatisticStore {
 
+	private static final Log log = LogFactory.getLog(CompletedStatisticStore.class);
+
 	/**
 	 * Completed statistics entries for message flows.
 	 */
-	private final List<PublishingFlow> completedStatisticEntries;
+	private final Queue<PublishingFlow> completedStatisticEntries;
+
+	/**
+	 * Queue size to keep
+	 */
+	private int queueSize;
 
 	public CompletedStatisticStore() {
-		completedStatisticEntries = new LinkedList<>();
 
+		completedStatisticEntries = new LinkedList<>();
+		queueSize = Integer.parseInt(SynapsePropertiesLoader.getPropertyValue(
+				StatisticsConstants.FLOW_STATISTICS_COMPLETED_STORE_QUEUE_SIZE,
+				StatisticsConstants.FLOW_STATISTICS_COMPLETED_STORE_DEFAULT_QUEUE_SIZE));
 	}
 
 	public void putCompletedStatisticEntry(PublishingFlow publishingFlow) {
-		synchronized (completedStatisticEntries) {
+		if (queueSize > completedStatisticEntries.size()) {
 			completedStatisticEntries.add(publishingFlow);
+		} else {
+			log.info("Completed statistic store queue has reached maximum size. Dropping incoming completed statistic" +
+			         " entries.");
 		}
 	}
 
-	public boolean isEmpty() {
-		return completedStatisticEntries.isEmpty();
-	}
-
-	public List<PublishingFlow> getCompletedStatisticEntries() {
-		List<PublishingFlow> cloneOfCompletedStatisticEntries = new LinkedList<>();
-		synchronized (completedStatisticEntries) {
-			cloneOfCompletedStatisticEntries.addAll(completedStatisticEntries);
-			completedStatisticEntries.clear();
-		}
-		return cloneOfCompletedStatisticEntries;
+	public Queue<PublishingFlow> getCompletedStatisticEntries() {
+		return completedStatisticEntries;
 	}
 }
