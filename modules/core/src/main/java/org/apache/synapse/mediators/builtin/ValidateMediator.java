fa408a4cbcba3e80dc8a3beaa5c6dba75244195b
Nuwan Wimalasekara <nuwanw@Nuwans-MacBook-Pro.local>
2016-06-21 22:15:39 +0530

    optimized imports

--- modules/core/src/main/java/org/apache/synapse/mediators/builtin/ValidateMediator.java
+++ modules/core/src/main/java/org/apache/synapse/mediators/builtin/ValidateMediator.java
@@ -22,7 +22,6 @@ package org.apache.synapse.mediators.builtin;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.fge.jackson.JsonLoader;
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
-import com.github.fge.jsonschema.core.report.ListProcessingReport;
 import com.github.fge.jsonschema.core.report.ProcessingMessage;
 import com.github.fge.jsonschema.core.report.ProcessingReport;
 import com.github.fge.jsonschema.main.JsonSchema;
