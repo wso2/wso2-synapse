8d03e78b2e439aa03a7773a5c4c15bed17cebe65
Vijitha Ekanayake <ekvijitha@gmail.com>
2016-07-29 10:16:11 +0530

    Fix for ESBJAVA-4760

--- modules/transports/core/nhttp/src/main/java/org/apache/synapse/transport/passthru/PassThroughConstants.java
+++ modules/transports/core/nhttp/src/main/java/org/apache/synapse/transport/passthru/PassThroughConstants.java
@@ -201,6 +201,7 @@ public class PassThroughConstants {
     public static final String HTTP_DELETE = "DELETE";
     public static final String HTTP_PUT = "PUT";
     public static final String HTTP_OPTIONS = "OPTIONS";
+    public static final String HTTP_CONNECT = "CONNECT";
 
     //Constant to specify the socket timeout
     public static final String HTTP_SOCKET_TIMEOUT = "HTTP_SOCKET_TIMEOUT";
