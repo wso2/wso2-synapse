ae6747abb9a4a23eeb0215afa8fc6fc1e7be3673
manoramahp <manorama@wso2.com>
2017-10-27 12:23:03 +0530

    Add test cases for XFormURLEncodedBuilder

---
+++ modules/commons/src/test/java/org/apache/synapse/commons/builders/XFormURLEncodedBuilderTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.synapse.commons.builders;
+
+import junit.framework.TestCase;
+import org.apache.axiom.om.OMElement;
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.Constants;
+import org.apache.axis2.addressing.EndpointReference;
+import org.apache.axis2.context.MessageContext;
+import org.apache.axis2.description.AxisBindingOperation;
+import org.apache.axis2.transport.http.HTTPConstants;
+import org.apache.axis2.util.MultipleEntryHashMap;
+import org.apache.synapse.commons.json.Util;
+
+/**
+ * Test class for XFormURLEncodedBuilder
+ */
+public class XFormURLEncodedBuilderTest extends TestCase {
+
+    /**
+     * Test XFormURLEncodedBuilder
+     * @throws AxisFault
+     */
+    public void testProcessDocument() throws AxisFault {
+        String expectedSoapEnvelope = "<?xml version='1.0' encoding='utf-8'?>" +
+                "<soapenv:Envelope xmlns:soapenv=\"http://www.w3.org/2003/05/soap-envelope\">" +
+                "<soapenv:Body />" +
+                "</soapenv:Envelope>";
+        MessageContext messageContext = Util.newMessageContext();
+        messageContext.setTo(new EndpointReference("http://localhost:9000/stockquote/test?symbol=WSO2&price=10"));
+        XFormURLEncodedBuilder urlEncodedBuilder = new XFormURLEncodedBuilder();
+        OMElement element = urlEncodedBuilder.processDocument(null, "", messageContext);
+        assertEquals("Invalid SOAPEnvelope received", expectedSoapEnvelope, element.toString());
+    }
+
+    /**
+     * Test XFormURLEncodedBuilder with HTTP POST method
+     * @throws AxisFault
+     */
+    public void testProcessDocumentPostMethod() throws AxisFault {
+        String expectedSoapEnvelope = "<?xml version='1.0' encoding='utf-8'?>" +
+                "<soapenv:Envelope xmlns:soapenv=\"http://www.w3.org/2003/05/soap-envelope\">" +
+                "<soapenv:Body>" +
+                "<xformValues>" +
+                "<price>10</price>" +
+                "<symbol>WSO2</symbol>" +
+                "</xformValues>" +
+                "</soapenv:Body>" +
+                "</soapenv:Envelope>";
+        MessageContext messageContext = Util.newMessageContext();
+        messageContext.setTo(new EndpointReference("http://localhost:9000/stockquote/test?symbol=WSO2&price=10"));
+        messageContext.setProperty(HTTPConstants.HTTP_METHOD, "POST");
+        XFormURLEncodedBuilder urlEncodedBuilder = new XFormURLEncodedBuilder();
+        OMElement element = urlEncodedBuilder.processDocument(null, "", messageContext);
+        assertEquals("Invalid SOAPEnvelope received", expectedSoapEnvelope, element.toString());
+    }
+
+    /**
+     * Test extracting parameters from 'whttp:location'
+     * @throws AxisFault
+     */
+    public void testExtractParametersUsingHttpLocation() throws AxisFault {
+        AxisBindingOperation axisBindingOperation = new AxisBindingOperation();
+        axisBindingOperation.setProperty("whttp:location", "stock/{symbol}");
+
+        MessageContext messageContext = Util.newMessageContext();
+        messageContext.setProperty("AxisBindingOperation", axisBindingOperation);
+        messageContext.setTo(new EndpointReference("http://localhost:9000/stockquote"));
+
+        XFormURLEncodedBuilder urlEncodedBuilder = new XFormURLEncodedBuilder();
+        urlEncodedBuilder.processDocument(null, "", messageContext);
+
+        assertNotNull("Parameter 'symbol' not found in request parameter map",
+                ((MultipleEntryHashMap)messageContext.getProperty(Constants.REQUEST_PARAMETER_MAP)).get("symbol"));
+    }
+}
