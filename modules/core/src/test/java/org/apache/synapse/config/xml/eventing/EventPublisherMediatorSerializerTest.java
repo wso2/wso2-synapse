efaee85e6680b50636541bc630c213bec5a1dd9d
Nirothipan <nirothipan@gmail.com>
2018-02-27 16:13:06 +0530

    Improve https://github.com/wso2/wso2-synapse/pull/924 based on comments

--- modules/core/src/test/java/org/apache/synapse/config/xml/eventing/EventPublisherMediatorSerializerTest.java
+++ modules/core/src/test/java/org/apache/synapse/config/xml/eventing/EventPublisherMediatorSerializerTest.java
@@ -1,20 +1,20 @@
 /*
-*  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.synapse.config.xml.eventing;
 
 import org.apache.axiom.om.OMElement;
@@ -35,7 +35,7 @@ public class EventPublisherMediatorSerializerTest {
      */
     @Test(expected = SynapseException.class)
     public void testCreateEventSource() {
-            EventPublisherMediatorSerializer mediatorSerializer = new EventPublisherMediatorSerializer();
+        EventPublisherMediatorSerializer mediatorSerializer = new EventPublisherMediatorSerializer();
         Mediator mediator = new CallMediator();
         mediatorSerializer.serializeSpecificMediator(mediator);
     }
@@ -59,8 +59,7 @@ public class EventPublisherMediatorSerializerTest {
         EventPublisherMediator mediator = new EventPublisherMediator();
         mediator.setEventSourceName("Test");
         OMElement element = mediatorSerializer.serializeSpecificMediator(mediator);
-        Assert.assertEquals("Name should be eventPublisher", "eventPublisher",
-                element.getLocalName());
+        Assert.assertEquals("Name should be eventPublisher", "eventPublisher", element.getLocalName());
     }
 }
 
