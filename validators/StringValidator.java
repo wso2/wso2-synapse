403cda2258164b2125c5cde033d0d3101ad1aaf5
GDLMadushanka <gdlmadushanka@gmail.com>
2018-10-06 18:11:23 +0530

    Add unit tests and enum support for StringValidator

--- validators/StringValidator.java
+++ validators/StringValidator.java
@@ -1,7 +1,6 @@
 package validators;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
+import com.google.gson.*;
 import contants.ValidatorConstants;
 import exceptions.ParserException;
 import exceptions.ValidatorException;
@@ -12,14 +11,33 @@ import utils.DataTypeConverter;
  */
 public class StringValidator {
 
+    // Use without instantiating.
+    private StringValidator() {
+    }
+
     private static int minLength;
     private static int maxLength;
 
     public static final String MIN_LENGTH = "minLength";
     public static final String MAX_LENGTH = "maxLength";
     public static final String STR_PATTERN = "pattern";
+    public static final String ENUM = "enum";
 
-    public static JsonPrimitive parseNominal(JsonObject inputObject, String value) throws ValidatorException, ParserException {
+    /**
+     * Validate a given string against its schema.
+     *
+     * @param inputObject json schema as an object.
+     * @param value       input string.
+     * @return if valid return a JsonPrimitive created using input string.
+     * @throws ValidatorException Didn't met validation criteria.
+     * @throws ParserException    Exception occurs in data type conversions.
+     */
+    public static JsonPrimitive parseNominal(JsonObject inputObject, String value) throws ValidatorException,
+            ParserException {
+        if (value == null) {
+            throw new ValidatorException("Expected string but found null");
+        }
+        // String length validations
         if (inputObject.has(MAX_LENGTH)) {
             String maxLengthString = inputObject.get(MAX_LENGTH).getAsString().replaceAll(ValidatorConstants.REGEX, "");
             if (!maxLengthString.isEmpty()) {
@@ -38,12 +56,27 @@ public class StringValidator {
                 }
             }
         }
+        // String pattern validations
         if (inputObject.has(STR_PATTERN)) {
             String patternString = inputObject.get(STR_PATTERN).getAsString().replaceAll(ValidatorConstants.REGEX, "");
-            if (!patternString.isEmpty()) {
-                if (!value.matches(patternString)) {
-                    throw new ValidatorException("String \"" + value + "\" violated the regex constraint " +
-                            patternString);
+            if (!patternString.isEmpty() && !value.matches(patternString)) {
+                throw new ValidatorException("String \"" + value + "\" violated the regex constraint " + patternString);
+            }
+        }
+        // Enum validations
+        // TODO : Improve logic to return at once
+        if (inputObject.has(ENUM)) {
+            boolean matchFound = false;
+            JsonArray enumElements = inputObject.getAsJsonArray(ENUM);
+            if (enumElements.size() > 0) {
+                for (JsonElement element : enumElements) {
+                    // valid if value matches with any enum
+                    if (value.equals(element.getAsString())) {
+                        matchFound = true;
+                    }
+                }
+                if (!matchFound) {
+                    throw new ValidatorException("String \"" + value + "\" not contains any element from the enum");
                 }
             }
         }
