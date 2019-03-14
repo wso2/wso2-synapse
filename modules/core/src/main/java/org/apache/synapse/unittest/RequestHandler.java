0785078bfd0cd9af8aa7d15538f5049cc8b8ad22
sajithaliyanage <sajithaliyanage@gmail.com>
2019-03-14 15:35:59 +0530

    Close TCP connection after all done

--- modules/core/src/main/java/org/apache/synapse/unittest/RequestHandler.java
+++ modules/core/src/main/java/org/apache/synapse/unittest/RequestHandler.java
@@ -56,6 +56,8 @@ public class RequestHandler implements Runnable {
 
         } catch (Exception e) {
             logger.error(e);
+        } finally {
+            closeSocket();
         }
     }
 
