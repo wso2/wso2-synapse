d1e7c21a0f6407c3a4438f9d1e55b4a1293bbe17
SachithK <sachithk@wso2.com>
2019-04-01 20:06:38 +0530

    Fix NPE in correlation logging This fixes a possible NPE when using correlation logging togther with JMS transport. Correlation_Id is not set for JMS messages but this property is referred in the target side when sending the message to a backend using the HTTP transport. Fixes : https://github.com/wso2/product-ei/issues/3577

--- modules/core/src/main/java/org/apache/synapse/util/MessageHelper.java
+++ modules/core/src/main/java/org/apache/synapse/util/MessageHelper.java
@@ -128,7 +128,8 @@ public class MessageHelper {
         //Correlation logging code starts here
         org.apache.axis2.context.MessageContext originalAxis2Ctx =
                 ((Axis2MessageContext) synCtx).getAxis2MessageContext();
-        if (originalAxis2Ctx.isPropertyTrue(PassThroughConstants.CORRELATION_LOG_STATE_PROPERTY)) {
+        if (originalAxis2Ctx.isPropertyTrue(PassThroughConstants.CORRELATION_LOG_STATE_PROPERTY)
+                && originalAxis2Ctx.getProperty(PassThroughConstants.CORRELATION_ID) != null) {
             String originalCorrelationId = originalAxis2Ctx.getProperty(PassThroughConstants.CORRELATION_ID).toString();
             axis2MC.getAxis2MessageContext().setProperty(PassThroughConstants.CORRELATION_ID, originalCorrelationId
                     + "_" + UUID.randomUUID().toString());
