7ce05729e1742e881c8797b656c7dcc4ea830812
fazlan-nazeem <fazlann@wso2.com>
2019-04-01 18:22:27 +0530

    Remove commented lines

--- modules/transports/core/nhttp/src/main/java/org/apache/synapse/transport/passthru/SourceHandler.java
+++ modules/transports/core/nhttp/src/main/java/org/apache/synapse/transport/passthru/SourceHandler.java
@@ -123,9 +123,7 @@ public class SourceHandler implements NHttpServerEventHandler {
     public void requestReceived(NHttpServerConnection conn) {
         try {
             HttpContext httpContext = conn.getContext();
-//            if (sourceConfiguration.isCorrelationLoggingEnabled()) {
-                setCorrelationId(conn);
-//            }
+            setCorrelationId(conn);
             httpContext.setAttribute(PassThroughConstants.REQ_ARRIVAL_TIME, System.currentTimeMillis());
             httpContext.setAttribute(PassThroughConstants.REQ_FROM_CLIENT_READ_START_TIME, System.currentTimeMillis());
             if (isMessageSizeValidationEnabled) {
