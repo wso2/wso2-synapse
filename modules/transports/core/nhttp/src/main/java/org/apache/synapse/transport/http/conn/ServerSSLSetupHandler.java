bc84fa51377a0adecff870b444d062fe18b5eb3a
Vathsan <srivathsanmail@gmail.com>
2019-11-14 14:40:37 +0530

    Improve code to not resolve hostname or ip unless required

--- modules/transports/core/nhttp/src/main/java/org/apache/synapse/transport/http/conn/ServerSSLSetupHandler.java
+++ modules/transports/core/nhttp/src/main/java/org/apache/synapse/transport/http/conn/ServerSSLSetupHandler.java
@@ -75,24 +75,24 @@ public class ServerSSLSetupHandler implements SSLSetupHandler {
     public void verify(
             final IOSession iosession,
             final SSLSession sslsession) throws SSLException {
-        SocketAddress remoteAddress = iosession.getRemoteAddress();
-        String address;
-        if (remoteAddress instanceof InetSocketAddress) {
-            InetSocketAddress inetSocketAddress = (InetSocketAddress) remoteAddress;
-            InetAddress internetAddress = inetSocketAddress.getAddress();
-            if (internetAddress != null) {
-                address = internetAddress.getHostAddress();
-            } else {
-                address = inetSocketAddress.getHostName();
-            }
-        } else {
-            address = remoteAddress.toString();
-        }
 
         if (verificationManager != null) {
             try {
                 verificationManager.verifyRevocationStatus(sslsession.getPeerCertificateChain());
             } catch (CertificateVerificationException e) {
+                SocketAddress remoteAddress = iosession.getRemoteAddress();
+                String address;
+                if (remoteAddress instanceof InetSocketAddress) {
+                    InetSocketAddress inetSocketAddress = (InetSocketAddress) remoteAddress;
+                    InetAddress internetAddress = inetSocketAddress.getAddress();
+                    if (internetAddress != null) {
+                        address = internetAddress.getHostAddress();
+                    } else {
+                        address = inetSocketAddress.getHostName();
+                    }
+                } else {
+                    address = remoteAddress.toString();
+                }
                 throw new SSLException("Certificate Chain Validation failed for host : " + address, e);
             }
         }
