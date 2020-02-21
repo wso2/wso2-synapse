9d3de7c511ddc557085d31dc269deb5057d0774b
Rosen Silva <rosen@live.com>
2020-02-21 14:39:41 +0530

    Allow System  variables to used in Endpoints

--- modules/core/src/main/java/org/apache/synapse/config/xml/endpoints/LoadbalanceEndpointFactory.java
+++ modules/core/src/main/java/org/apache/synapse/config/xml/endpoints/LoadbalanceEndpointFactory.java
@@ -160,16 +160,13 @@ public final class LoadbalanceEndpointFactory extends EndpointFactory {
             memberIter.hasNext();){
             OMElement memberEle = (OMElement) memberIter.next();
             String memberHostname = memberEle.getAttributeValue(new QName("hostName"));
-            memberHostname = ResolverFactory.getInstance().getResolver(memberHostname).resolve();
             Member member = new Member(memberHostname, -1);
             String http = memberEle.getAttributeValue(new QName("httpPort"));
             if (http != null) {
-                http = ResolverFactory.getInstance().getResolver(http).resolve();
                 member.setHttpPort(Integer.parseInt(http));
             }
             String https = memberEle.getAttributeValue(new QName("httpsPort"));
             if (https != null && https.trim().length() != 0) {
-                https = ResolverFactory.getInstance().getResolver(https).resolve();
                 member.setHttpsPort(Integer.parseInt(https.trim()));
             }
             members.add(member);
