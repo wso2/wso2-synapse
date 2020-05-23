c1abd3639b1610e92aad284bc6ab2933f96b1a35
Indika Sampath <indikasampath2000@gmail.com>
2020-05-23 18:28:28 +0530

    Refactor rabbitmq store-forward implementation
    - update the amqp-client library to the latest version
    - the producer changed to use the publisher confirms for reliable publishing
    - the consumer revamp according to the latest amqp-client version

--- modules/core/src/main/java/org/apache/synapse/message/store/impl/rabbitmq/RabbitMQConsumer.java
+++ modules/core/src/main/java/org/apache/synapse/message/store/impl/rabbitmq/RabbitMQConsumer.java
@@ -1,13 +1,13 @@
 /**
- *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- *
- *  WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * <p>
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -34,255 +34,225 @@ import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 
+/**
+ * The message consumer responsible for read a message from the queue and set it into the message context
+ */
 public class RabbitMQConsumer implements MessageConsumer {
-	private static final Log logger = LogFactory.getLog(RabbitMQConsumer.class.getName());
 
-	private Connection connection;
+    private static final Log log = LogFactory.getLog(RabbitMQConsumer.class.getName());
 
-	private Channel channel;
+    private Connection connection;
+    private Channel channel;
+    private RabbitMQStore store;
+    private String queueName;
+    private String idString;
+    private CachedMessage cachedMessage; // Holds the last message read from the message store
 
-	private RabbitMQStore store;
+    public RabbitMQConsumer(RabbitMQStore store) {
+        if (store == null) {
+            log.error("Cannot initialize consumer: " + getId());
+            return;
+        }
+        this.store = store;
+        cachedMessage = new CachedMessage();
+    }
 
-	private String queueName;
-
-	private String idString;
-
-	private boolean isInitialized;
-	/**
-	 * Did last receive() call cause an error?
-	 */
-	private boolean isReceiveError;
-	/**
-	 * Holds the last message read from the message store.
-	 */
-	private CachedMessage cachedMessage;
-
-	public RabbitMQConsumer(RabbitMQStore store) {
-		if (store == null) {
-			logger.error("Cannot initialize.");
-			return;
-		}
-		this.store = store;
-		cachedMessage = new CachedMessage();
-		isReceiveError = false;
-		isInitialized = true;
-	}
-
-	public MessageContext receive() {
-		if (!checkConnection()) {
-			if (!reconnect()) {
-				if (logger.isDebugEnabled()) {
-					logger.debug(getId()
-					             + " cannot receive message from store. Can not reconnect.");
-				}
-				return null;
-			} else {
-				logger.info(getId() + " reconnected to store.");
-				isReceiveError = false;
-			}
-		}
-		//setting channel
-		if (channel != null) {
-			if (!channel.isOpen()) {
-				if (!setChannel()) {
-					logger.info(getId() + " unable to create the channel.");
-					return null;
-				}
-			}
-		} else {
-			if (!setChannel()) {
-				logger.info(getId() + " unable to create the channel.");
-				return null;
-			}
-		}
-		//receive messages
-		try {
-
-			GetResponse delivery = null;
-			delivery = channel.basicGet(queueName, false);
-
-			if (delivery != null) {
-				//deserilizing message
-				StorableMessage storableMessage = null;
-				ByteArrayInputStream bis = new ByteArrayInputStream(delivery.getBody());
-				ObjectInput in = new ObjectInputStream(bis);
-				try {
-					storableMessage = (StorableMessage) in.readObject();
-				} catch (ClassNotFoundException e) {
-					logger.error(getId() + "unable to read the stored message" + e);
-					channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
-				}
-
-				bis.close();
-				in.close();
-				org.apache.axis2.context.MessageContext axis2Mc = store.newAxis2Mc();
-				MessageContext synapseMc = store.newSynapseMc(axis2Mc);
-				synapseMc =
-						MessageConverter.toMessageContext(storableMessage, axis2Mc, synapseMc);
-				updateCache(delivery, synapseMc, null, false);
-				if (logger.isDebugEnabled()) {
-					logger.debug(getId() + " Received MessageId:" +
-					             delivery.getProps().getMessageId());
-				}
-				return synapseMc;
-			}
-		} catch (ShutdownSignalException sse) {
-			logger.error(getId() + " connection error when receiving messages" + sse);
-		} catch (IOException ioe) {
-			logger.error(getId() + " connection error when receiving messages" + ioe);
-		}
-		return null;
-	}
-
-	public boolean ack() {
-		boolean result = cachedMessage.ack();
-		if (result) {
-			store.dequeued();
-		}
-		return result;
-	}
-
-	public boolean cleanup() {
-		if (logger.isDebugEnabled()) {
-			logger.debug(getId() + " cleaning up...");
-		}
-		boolean result = store.cleanup(connection, true);
-		if (result) {
-			connection = null;
-			return true;
-		}
-		return false;
-	}
-
+    /**
+     * Get a single message from the queue and deserialize for set into the message context
+     *
+     * @return the {@link MessageContext} with received message
+     */
     @Override
-    public boolean isAlive() {
+    public MessageContext receive() {
+        if (isAlive()) {
+            GetResponse delivery = null;
+            try {
+                delivery = channel.basicGet(queueName, false);
+                if (delivery != null) {
+                    StorableMessage storableMessage = deserializeMessage(delivery);
+                    org.apache.axis2.context.MessageContext axis2Mc = store.newAxis2Mc();
+                    MessageContext synapseMc = store.newSynapseMc(axis2Mc);
+                    synapseMc = MessageConverter.toMessageContext(storableMessage, axis2Mc, synapseMc);
+                    updateCache(delivery, delivery.getProps().getMessageId());
+                    if (log.isDebugEnabled()) {
+                        log.debug(getId() + " Received MessageId: " + delivery.getProps().getMessageId());
+                    }
+                    return synapseMc;
+                }
+            } catch (ShutdownSignalException | IOException e) {
+                log.error(getId() + " connection error when receiving messages.", e);
+                cleanup();
+            } catch (ClassNotFoundException e) {
+                log.error(getId() + "unable to read the stored message.", e);
+                try {
+                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                } catch (IOException ex) {
+                    log.error(getId() + "unable to acknowledge the stored message.", e);
+                }
+            }
+        } else {
+            log.warn("The connection and channel to the RabbitMQ broker are unhealthy.");
+            cleanup();
+            setConnection(store.createConnection());
+            setChannel(store.createChannel(connection));
+        }
+        return null;
+    }
+
+    /**
+     * Deserialize the message taken from the queue
+     *
+     * @param delivery the message received from the broker
+     * @return a {@link StorableMessage} object
+     * @throws IOException
+     */
+    private StorableMessage deserializeMessage(GetResponse delivery) throws IOException, ClassNotFoundException {
+        StorableMessage storableMessage;
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(delivery.getBody());
+             ObjectInput objectInput = new ObjectInputStream(inputStream)) {
+            storableMessage = (StorableMessage) objectInput.readObject();
+        }
+        return storableMessage;
+    }
+
+    /**
+     * Acknowledge the message upon successful backend invocation
+     *
+     * @return whether message is successfully acknowledge or not
+     */
+    @Override
+    public boolean ack() {
+        boolean result = cachedMessage.ack();
+        if (result) {
+            store.dequeued();
+        }
+        return result;
+    }
+
+    /**
+     * Cleanup the {@link Connection} and it's related resources
+     *
+     * @return whether cleanup successful or not
+     */
+    @Override
+    public boolean cleanup() {
+        if (connection != null) {
+            connection.abort();
+        }
+        channel = null;
+        connection = null;
         return true;
     }
 
-    public RabbitMQConsumer setConnection(Connection connection) {
-		this.connection = connection;
-		return this;
-	}
+    /**
+     * Check availability of connectivity with the message store
+     *
+     * @return {@code true} if connection available, {@code false} otherwise.
+     */
+    @Override
+    public boolean isAlive() {
+        return (connection != null && connection.isOpen()) && (channel != null && channel.isOpen());
+    }
 
-	public void setQueueName(String queueName) {
-		this.queueName = queueName;
-	}
+    /**
+     * Get ID of this RabbitMQ consumer
+     *
+     * @return the ID
+     */
+    @Override
+    public String getId() {
+        return idString;
+    }
 
-	public boolean setChannel() {
-		if (connection != null && connection.isOpen()) {
-			try {
-				this.channel = connection.createChannel();
-				return true;
-			} catch (IOException e) {
-				return false;
-			}
-		}
-		return false;
-	}
+    /**
+     * Set ID of this RabbitMQ consumer
+     *
+     * @param id ID
+     */
+    @Override
+    public void setId(int id) {
+        idString = "[" + store.getName() + "-C-" + id + "]";
+    }
 
-	public void setId(int id) {
-		idString = "[" + store.getName() + "-C-" + id + "]";
-	}
+    /**
+     * Set the {@link Connection} object
+     *
+     * @param connection a {@link Connection} object
+     */
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
 
-	public String getId() {
-		return idString;
-	}
+    /**
+     * Set the {@link Channel} object
+     *
+     * @param channel a {@link Channel} object
+     */
+    public void setChannel(Channel channel) {
+        this.channel = channel;
+    }
 
-	public Connection getConnection() {
-		return connection;
-	}
+    /**
+     * Set the queue name
+     *
+     * @param queueName the queue name
+     */
+    public void setQueueName(String queueName) {
+        this.queueName = queueName;
+    }
 
-	private boolean checkConnection() {
-		if (connection == null) {
-			if (logger.isDebugEnabled()) {
-				logger.debug(getId() + " cannot proceed. RabbitMQ Connection is null.");
-			}
-			return false;
-		}
-		if (!connection.isOpen()) {
-			if (logger.isDebugEnabled()) {
-				logger.debug(getId() + " cannot proceed. RabbitMQ Connection is closed.");
-			}
-			return false;
-		}
-		return true;
-	}
+    /**
+     * Update the cached message with the message received from the queue
+     *
+     * @param delivery  the received message from the queue
+     * @param messageId the message id
+     */
+    private void updateCache(GetResponse delivery, String messageId) {
+        cachedMessage.setMessage(delivery);
+        cachedMessage.setId(messageId);
+    }
 
-	private boolean reconnect() {
-		RabbitMQConsumer consumer = (RabbitMQConsumer) store.getConsumer();
+    /**
+     * This is used to store the last received message
+     * if the message is successfully sent to the endpoint, ack will sent to the store
+     * in order to delete message from the queue
+     * <p/>
+     * In RabbitMQ message ack should be using the same channel which was consumed the message
+     * There for the consumed channel will also stored without closing until the message is ackd
+     */
+    private final class CachedMessage {
+        private GetResponse message = null;
+        private String id = "";
 
-		if (consumer.getConnection() == null) {
-			if (logger.isDebugEnabled()) {
-				logger.debug(getId() + " could not reconnect to the broker.");
-			}
-			return false;
-		}
-		setConnection(consumer.getConnection());
-		if (logger.isDebugEnabled()) {
-			logger.debug(getId() + " ===> " + consumer.getId());
-		}
-		idString = consumer.getId();
-		return true;
-	}
+        public void setMessage(GetResponse message) {
+            this.message = message;
+        }
 
-	private void updateCache(GetResponse delivery, MessageContext synCtx,
-	                         String messageId,
-	                         boolean receiveError) throws IOException {
-		isReceiveError = receiveError;
-		cachedMessage.setMessage(delivery);
-		cachedMessage.setMc(synCtx);
-		cachedMessage.setId(messageId);
-	}
+        public boolean ack() {
+            if (message != null && channel != null && channel.isOpen()) {
+                try {
+                    channel.basicAck(message.getEnvelope().getDeliveryTag(), false);
+                    return true;
+                } catch (IOException e) {
+                    log.error(getId() + " cannot ack last read message. Error: " + e.getLocalizedMessage(), e);
+                    return false;
+                }
+            }
+            return false;
+        }
 
-	/**
-	 * This is used to store the last received message
-	 * if the message is successfully sent to the endpoint, ack will sent to the store
-	 * in order to delete message from the queue
-	 * <p/>
-	 * In RabbitMQ message ack should be using the same channel which was consumed the message
-	 * There for the consumed channel will also stored without closing until the message is ackd
-	 */
-	private final class CachedMessage {
-		private GetResponse message = null;
-		private MessageContext mc = null;
-		private String id = "";
+        public GetResponse getMessage() {
+            return message;
+        }
 
-		public CachedMessage setMessage(GetResponse message) {
-			this.message = message;
-			return this;
-		}
+        public CachedMessage setId(String id) {
+            this.id = id;
+            return this;
+        }
 
-		public boolean ack() {
-			if (message != null && channel != null && channel.isOpen()) {
-				try {
-					channel.basicAck(message.getEnvelope().getDeliveryTag(), false);
-					return true;
-				} catch (IOException e) {
-					logger.error(getId() + " cannot ack last read message. Error:"
-					             + e.getLocalizedMessage(), e);
-					return false;
-				}
-			}
-			return false;
-		}
-
-		public GetResponse getMessage() {
-			return message;
-		}
-
-		public CachedMessage setMc(MessageContext mc) {
-			this.mc = mc;
-			return this;
-		}
-
-		public CachedMessage setId(String id) {
-			this.id = id;
-			return this;
-		}
-
-		public String getId() {
-			return id;
-		}
-	}
+        public String getId() {
+            return id;
+        }
+    }
 }
 
