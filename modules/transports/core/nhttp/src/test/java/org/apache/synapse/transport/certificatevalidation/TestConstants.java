6449cd6468cbdc162c2259148766eea143f515a1
Dilan Tharaka <tharakamd6@gmail.com>
2020-06-08 08:18:48 +0530

    github certificate update

--- modules/transports/core/nhttp/src/test/java/org/apache/synapse/transport/certificatevalidation/TestConstants.java
+++ modules/transports/core/nhttp/src/test/java/org/apache/synapse/transport/certificatevalidation/TestConstants.java
@@ -21,27 +21,27 @@ package org.apache.synapse.transport.certificatevalidation;
 public interface TestConstants {
 
     //Validity period of a fake certificate made. 1 day (in milliseconds)
-    final static int VALIDITY_PERIOD = 24 * 60 * 60 * 1000;
+    int VALIDITY_PERIOD = 24 * 60 * 60 * 1000;
     //Next update for OCSPResponse or X509CRL will be after Now + NEXT_UPDATE_PERIOD
-    final static int NEXT_UPDATE_PERIOD = 1000000;
+    int NEXT_UPDATE_PERIOD = 1000000;
 
     /**
      * The certificates in the resources folder will contain the certificates in the certificate chain from
      * https://www.github.com
      * These certificates are chosen because the certificate issuers support both CRL and OCSP. Read the certificates for
      * more details.
-     *
+     * <p>
      * CAUTION: Replace the certificates if they expire or are marked as revoked by their issuers. At the moment they are
      * valid. The expiry dates of the certificates are as follows:
-     *
-     * github.com                    : 6/3/2020
+     * <p>
+     * github.com                    : 5/10/2022
      * DigiCertHighAssuranceEVCA-1   : 10/22/2028
      * DigiCertHighAssuranceEVRootCA : 11/10/2031
      */
-    final static String REAL_PEER_CERT = "/org/apache/synapse/transport/certificatevalidation" +
+    String REAL_PEER_CERT = "/org/apache/synapse/transport/certificatevalidation" +
             "/certificates/github/github.com";
-    final static String INTERMEDIATE_CERT = "/org/apache/synapse/transport/certificatevalidation" +
+    String INTERMEDIATE_CERT = "/org/apache/synapse/transport/certificatevalidation" +
             "/certificates/github/DigiCertHighAssuranceEVCA-1";
-    final static String ROOT_CERT = "/org/apache/synapse/transport/certificatevalidation" +
+    String ROOT_CERT = "/org/apache/synapse/transport/certificatevalidation" +
             "/certificates/github/DigiCertHighAssuranceEVRootCA";
 }
