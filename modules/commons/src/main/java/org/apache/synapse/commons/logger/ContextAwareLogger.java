7d3eb5d932a46144801865bad0e8b7c5a4820470
Tharindu Dharmarathna <tharindua@wso2.com>
2021-04-01 20:47:58 +0530

    fix testcases

--- modules/commons/src/main/java/org/apache/synapse/commons/logger/ContextAwareLogger.java
+++ modules/commons/src/main/java/org/apache/synapse/commons/logger/ContextAwareLogger.java
@@ -78,4 +78,8 @@ public class ContextAwareLogger {
         return log;
     }
 
+    protected static void setCorrelationLoggingEnabled(Boolean correlationLoggingEnabled) {
+
+        ContextAwareLogger.correlationLoggingEnabled = correlationLoggingEnabled;
+    }
 }
