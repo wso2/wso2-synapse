1a2325801f1f2a8d76da8581045f3646accbf49d
chanikag <chanika@wso2.com>
2021-07-02 15:10:33 +0530

    Add testcases for EI5459
    This is to add testcase for https://github.com/wso2/product-ei/issues/5459

--- modules/integration/src/test/java/org/apache/synapse/samples/framework/tests/endpoint/Sample63.java
+++ modules/integration/src/test/java/org/apache/synapse/samples/framework/tests/endpoint/Sample63.java
@@ -72,4 +72,11 @@ public class Sample63 extends SynapseTestCase {
         HttpResponse response = client.doGet("http://127.0.0.1:8280/foodapi/list/refreshTokenGrant");
         assertEquals(HttpStatus.SC_OK, response.getStatus());
     }
+
+    public void testOAuthConfiguredEPWithoutTransportHeaders() throws Exception {
+
+        BasicHttpClient client = new BasicHttpClient();
+        HttpResponse response = client.doGet("http://127.0.0.1:8280/foodapi/list/withoutTransportHeaders");
+        assertEquals(HttpStatus.SC_OK, response.getStatus());
+    }
 }
