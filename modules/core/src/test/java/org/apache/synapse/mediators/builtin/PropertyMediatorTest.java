bc3889345af9096f5556c9601335c9a465f65493
Sulochana Kodituwakku <sulochana@wso2.com>
2022-07-27 12:28:17 +0530

    Implement testcases for Analytics scope

--- modules/core/src/test/java/org/apache/synapse/mediators/builtin/PropertyMediatorTest.java
+++ modules/core/src/test/java/org/apache/synapse/mediators/builtin/PropertyMediatorTest.java
@@ -22,10 +22,12 @@ package org.apache.synapse.mediators.builtin;
 import junit.framework.TestCase;
 import org.apache.http.protocol.HTTP;
 import org.apache.synapse.MessageContext;
+import org.apache.synapse.config.SynapseConfiguration;
 import org.apache.synapse.core.axis2.Axis2MessageContext;
 import org.apache.synapse.config.xml.XMLConfigConstants;
 import org.apache.synapse.mediators.MediatorProperty;
 import org.apache.synapse.mediators.TestUtils;
+import org.apache.synapse.mediators.base.SequenceMediator;
 import org.apache.synapse.util.xpath.SynapseXPath;
 import org.apache.axiom.om.OMElement;
 
@@ -228,6 +230,98 @@ public class PropertyMediatorTest extends TestCase {
 
         assertTrue(
             "value".equals(medProp.getEvaluatedExpression(synCtx)));
+    }
+
+    public void testAnalyticsMetadata() throws Exception {
+        SequenceMediator seq = new SequenceMediator();
+        seq.setName("SEQUENCE_ANALYTICS_UT");
+
+        PropertyMediator analyticBoolean = new PropertyMediator();
+        analyticBoolean.setName("analyticBoolean");
+        analyticBoolean.setAction(PropertyMediator.ACTION_SET);
+        analyticBoolean.setScope(XMLConfigConstants.SCOPE_ANALYTICS);
+        analyticBoolean.setValue(String.valueOf(true), XMLConfigConstants.DATA_TYPES.BOOLEAN.name());
+
+        PropertyMediator analyticDouble = new PropertyMediator();
+        analyticDouble.setName("analyticDouble");
+        analyticDouble.setAction(PropertyMediator.ACTION_SET);
+        analyticDouble.setScope(XMLConfigConstants.SCOPE_ANALYTICS);
+        analyticDouble.setValue(String.valueOf(Double.MAX_VALUE), XMLConfigConstants.DATA_TYPES.DOUBLE.name());
+
+        PropertyMediator analyticFloat = new PropertyMediator();
+        analyticFloat.setName("analyticFloat");
+        analyticFloat.setAction(PropertyMediator.ACTION_SET);
+        analyticFloat.setScope(XMLConfigConstants.SCOPE_ANALYTICS);
+        analyticFloat.setValue(String.valueOf(Float.MAX_VALUE), XMLConfigConstants.DATA_TYPES.FLOAT.name());
+
+        PropertyMediator analyticInteger = new PropertyMediator();
+        analyticInteger.setName("analyticInteger");
+        analyticInteger.setAction(PropertyMediator.ACTION_SET);
+        analyticInteger.setScope(XMLConfigConstants.SCOPE_ANALYTICS);
+        analyticInteger.setValue(String.valueOf(Integer.MAX_VALUE), XMLConfigConstants.DATA_TYPES.INTEGER.name());
+
+        PropertyMediator analyticLong = new PropertyMediator();
+        analyticLong.setName("analyticLong");
+        analyticLong.setAction(PropertyMediator.ACTION_SET);
+        analyticLong.setScope(XMLConfigConstants.SCOPE_ANALYTICS);
+        analyticLong.setValue(String.valueOf(Long.MAX_VALUE), XMLConfigConstants.DATA_TYPES.LONG.name());
+
+        PropertyMediator analyticShort = new PropertyMediator();
+        analyticShort.setName("analyticShort");
+        analyticShort.setAction(PropertyMediator.ACTION_SET);
+        analyticShort.setScope(XMLConfigConstants.SCOPE_ANALYTICS);
+        analyticShort.setValue(String.valueOf(Short.MAX_VALUE), XMLConfigConstants.DATA_TYPES.SHORT.name());
+
+        PropertyMediator analyticString = new PropertyMediator();
+        analyticString.setName("analyticString");
+        analyticString.setAction(PropertyMediator.ACTION_SET);
+        analyticString.setScope(XMLConfigConstants.SCOPE_ANALYTICS);
+        analyticString.setValue("WSO2", XMLConfigConstants.DATA_TYPES.STRING.name());
+
+        seq.addChild(analyticBoolean);
+        seq.addChild(analyticDouble);
+        seq.addChild(analyticFloat);
+        seq.addChild(analyticInteger);
+        seq.addChild(analyticLong);
+        seq.addChild(analyticShort);
+        seq.addChild(analyticString);
+
+        SynapseConfiguration synapseConfig = new SynapseConfiguration();
+        MessageContext synCtx = TestUtils.createSynapseMessageContext("<foo/>", synapseConfig);
+        Axis2MessageContext messageContext = (Axis2MessageContext) synCtx;
+        seq.mediate(messageContext);
+
+        assertTrue(messageContext.getAnalyticsMetadata().containsKey("analyticBoolean"));
+        assertTrue(Boolean.parseBoolean(messageContext.getAnalyticsMetadata().get("analyticBoolean").toString()));
+
+        assertTrue(messageContext.getAnalyticsMetadata().containsKey("analyticDouble"));
+        assertEquals(Double.MAX_VALUE, messageContext.getAnalyticsMetadata().get("analyticDouble"));
+
+        assertTrue(messageContext.getAnalyticsMetadata().containsKey("analyticFloat"));
+        assertEquals(Float.MAX_VALUE, messageContext.getAnalyticsMetadata().get("analyticFloat"));
+
+        assertTrue(messageContext.getAnalyticsMetadata().containsKey("analyticInteger"));
+        assertEquals(Integer.MAX_VALUE, messageContext.getAnalyticsMetadata().get("analyticInteger"));
+
+        assertTrue(messageContext.getAnalyticsMetadata().containsKey("analyticLong"));
+        assertEquals(Long.MAX_VALUE, messageContext.getAnalyticsMetadata().get("analyticLong"));
+
+        assertTrue(messageContext.getAnalyticsMetadata().containsKey("analyticShort"));
+        assertEquals(Short.MAX_VALUE, messageContext.getAnalyticsMetadata().get("analyticShort"));
+
+        assertTrue(messageContext.getAnalyticsMetadata().containsKey("analyticString"));
+        assertEquals("WSO2", messageContext.getAnalyticsMetadata().get("analyticString"));
+
+        analyticBoolean.setAction(PropertyMediator.ACTION_REMOVE);
+        analyticDouble.setAction(PropertyMediator.ACTION_REMOVE);
+        analyticFloat.setAction(PropertyMediator.ACTION_REMOVE);
+        analyticInteger.setAction(PropertyMediator.ACTION_REMOVE);
+        analyticLong.setAction(PropertyMediator.ACTION_REMOVE);
+        analyticShort.setAction(PropertyMediator.ACTION_REMOVE);
+        analyticString.setAction(PropertyMediator.ACTION_REMOVE);
+
+        seq.mediate(messageContext);
+        assertTrue( messageContext.getAnalyticsMetadata().isEmpty());
     }
 
 }
