0328c2cb5c6fad70d4c6a378f39757942f451a64
Sanoj Punchihewa <sanoj.punchihewa@gmail.com>
2023-04-25 13:46:10 +0530

    Fix review comments
    Add a null check for TransportOutDescription and TransportSender

--- modules/core/src/main/java/org/apache/synapse/core/axis2/TimeoutHandler.java
+++ modules/core/src/main/java/org/apache/synapse/core/axis2/TimeoutHandler.java
@@ -21,6 +21,7 @@ package org.apache.synapse.core.axis2;
 
 import org.apache.axiom.om.OMAbstractFactory;
 import org.apache.axiom.soap.SOAPEnvelope;
+import org.apache.axis2.description.TransportOutDescription;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.synapse.FaultHandler;
@@ -233,8 +234,11 @@ public class TimeoutHandler extends TimerTask {
                     if (RuntimeStatisticCollector.isStatisticsEnabled()) {
                         CallbackStatisticCollector.callbackCompletionEvent(callback.getSynapseOutMsgCtx(), (String) key);
                     }
-                    // Call the TransportSender's onAppError method to release any resources
-                    callback.getAxis2OutMsgCtx().getTransportOut().getSender().onAppError(callback.getAxis2OutMsgCtx());
+                    TransportOutDescription transportOut = callback.getAxis2OutMsgCtx().getTransportOut();
+                    if (transportOut != null && transportOut.getSender() != null) {
+                        // Call the TransportSender's onAppError method to release any resources
+                        transportOut.getSender().onAppError(callback.getAxis2OutMsgCtx());
+                    }
                 }
             }
         }
