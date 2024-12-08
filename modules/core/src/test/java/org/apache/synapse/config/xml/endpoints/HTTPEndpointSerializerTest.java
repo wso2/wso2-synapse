c88988f08c607a0d6b05a61eef4bdc3710f82206
chathurangaj <chathuranga.jayanath.1999@gmail.com>
2024-12-08 22:48:36 +0530

    add serializer tests

--- modules/core/src/test/java/org/apache/synapse/config/xml/endpoints/HTTPEndpointSerializerTest.java
+++ modules/core/src/test/java/org/apache/synapse/config/xml/endpoints/HTTPEndpointSerializerTest.java
@@ -33,4 +33,78 @@ public class HTTPEndpointSerializerTest extends AbstractTestCase {
         OMElement serializedResponse = HTTPEndpointSerializer.getElementFromEndpoint(endpoint);
         assertTrue("Endpoint not serialized!", compare(serializedResponse, inputElement));
     }
+
+    public void testCreateEndpointDefinitionWithErrorHandling() throws Exception {
+
+        String inputXml = "<endpoint  xmlns=\"http://ws.apache.org/ns/synapse\">" +
+                "<http method=\"GET\" uri-template=\"http://localhost:3000/banks\">" +
+                    "<timeout>" +
+                        "<duration>1000</duration>" +
+                        "<responseAction>discard</responseAction>" +
+                    "</timeout>" +
+                    "<suspendOnFailure>" +
+                        "<errorCodes>403</errorCodes>" +
+                        "<progressionFactor>1</progressionFactor>" +
+                    "</suspendOnFailure>" +
+                "</http>" +
+                "</endpoint>";
+        OMElement inputElement = AXIOMUtil.stringToOM(inputXml);
+        HTTPEndpoint endpoint = (HTTPEndpoint) HTTPEndpointFactory.getEndpointFromElement(inputElement, true, null);
+        OMElement serializedResponse = HTTPEndpointSerializer.getElementFromEndpoint(endpoint);
+        assertTrue("Endpoint definition with error handling not serialized!", compare(serializedResponse, inputElement));
+    }
+
+    public void testCreateEndpointDefinitionWithCompleteErrorHandling() throws Exception {
+
+        String inputXml = "<endpoint  xmlns=\"http://ws.apache.org/ns/synapse\">" +
+                "<http method=\"GET\" uri-template=\"http://localhost:3000/banks\">" +
+                    "<timeout>" +
+                        "<duration>200</duration>" +
+                        "<responseAction>discard</responseAction>" +
+                    "</timeout>" +
+                    "<suspendOnFailure>" +
+                        "<errorCodes>403</errorCodes>" +
+                        "<initialDuration>10</initialDuration>" +
+                        "<maximumDuration>10</maximumDuration>" +
+                        "<progressionFactor>2</progressionFactor>" +
+                    "</suspendOnFailure>" +
+                    "<markForSuspension>" +
+                        "<errorCodes>304</errorCodes>"+
+                        "<retriesBeforeSuspension>10</retriesBeforeSuspension>" +
+                        "<retryDelay>5</retryDelay>" +
+                    "</markForSuspension>" +
+                "</http>" +
+                "</endpoint>";
+        OMElement inputElement = AXIOMUtil.stringToOM(inputXml);
+        HTTPEndpoint endpoint = (HTTPEndpoint) HTTPEndpointFactory.getEndpointFromElement(inputElement, true, null);
+        OMElement serializedResponse = HTTPEndpointSerializer.getElementFromEndpoint(endpoint);
+        assertTrue("Endpoint definition with complete error handling not serialized!", compare(serializedResponse, inputElement));
+    }
+
+    public void testCreateEndpointDefinitionWithDynamicErrorHandling() throws Exception {
+
+        String inputXml = "<endpoint  xmlns=\"http://ws.apache.org/ns/synapse\">" +
+                "<http method=\"GET\" uri-template=\"http://localhost:3000/banks\">" +
+                    "<timeout>" +
+                        "<duration>{get-property('timeoutDuration')}</duration>" +
+                        "<responseAction>{get-property('timeoutAction')}</responseAction>" +
+                    "</timeout>" +
+                    "<suspendOnFailure>" +
+                        "<errorCodes>{get-property('suspendErrorCodes')}</errorCodes>" +
+                        "<initialDuration>{get-property('suspendInitialDuration')}</initialDuration>" +
+                        "<maximumDuration>{get-property('suspendMaximumDuration')}</maximumDuration>" +
+                        "<progressionFactor>{get-property('suspendProgressionFactor')}</progressionFactor>" +
+                    "</suspendOnFailure>" +
+                    "<markForSuspension>" +
+                        "<errorCodes>{get-property('retryErrorCodes')}</errorCodes>"+
+                        "<retriesBeforeSuspension>{get-property('retryCount')}</retriesBeforeSuspension>" +
+                        "<retryDelay>{get-property('retryDelay')}</retryDelay>" +
+                    "</markForSuspension>" +
+                "</http>" +
+                "</endpoint>";
+        OMElement inputElement = AXIOMUtil.stringToOM(inputXml);
+        HTTPEndpoint endpoint = (HTTPEndpoint) HTTPEndpointFactory.getEndpointFromElement(inputElement, true, null);
+        OMElement serializedResponse = HTTPEndpointSerializer.getElementFromEndpoint(endpoint);
+        assertTrue("Endpoint definition with dynamic error handling not serialized!", compare(serializedResponse, inputElement));
+    }
 }
