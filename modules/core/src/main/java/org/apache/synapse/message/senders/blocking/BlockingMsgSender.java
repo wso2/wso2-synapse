df82a8e5c0826e38cac1093909db7c25178fbedb
DedunuKarunarathne <46235093+DedunuKarunarathne@users.noreply.github.com>
2024-12-19 11:05:20 +0530

    Fix NO_KEEPALIVE property not working issue with call blocking = true
    Fix NO_KEEPALIVE property not working issue with call blocking = true. Fixes:wso2/product-micro-integrator#3619

--- modules/core/src/main/java/org/apache/synapse/message/senders/blocking/BlockingMsgSender.java
+++ modules/core/src/main/java/org/apache/synapse/message/senders/blocking/BlockingMsgSender.java
@@ -348,6 +348,8 @@ public class BlockingMsgSender {
                 axisInMsgCtx.getProperty(SynapseConstants.NO_KEEPALIVE));
         axisOutMsgCtx.setProperty(SynapseConstants.NO_DEFAULT_CONTENT_TYPE,
                 axisInMsgCtx.getProperty(SynapseConstants.NO_DEFAULT_CONTENT_TYPE));
+        axisOutMsgCtx.setProperty(SynapseConstants.NO_KEEPALIVE,
+                axisInMsgCtx.getProperty(SynapseConstants.NO_KEEPALIVE));
         // Fill MessageContext
         BlockingMsgSenderUtils.fillMessageContext(endpointDefinition, axisOutMsgCtx, synapseInMsgCtx);
         if (JsonUtil.hasAJsonPayload(axisInMsgCtx)) {
